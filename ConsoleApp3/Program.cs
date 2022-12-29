3827f5d44cd3604d233b6611331faa31cd803fe1
barinskaya <1388zxc@gmail.com>
2022-12-29 10:52:04 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+	static void Main()
+	{
+		string a1 = "Error: division by zero!"; //задание текста ошибки 1
+		string a2 = "Error in input!"; //задание текста ошибки 2
+		int N = Convert.ToInt32(Console.ReadLine()); //ввод числа с консоли
+		if (N == 0) //выполнить, если введен 0
+		{
+			Console.WriteLine($"{a1}");//вывод ошибки
+		} 
+		else //другой исход
+		{
+			Console.WriteLine($"{a2}");// вывод ошибки
+		}		
+		Console.ReadLine();//прогорамма ждет нажатия клавиши для завершения работы
+	}
+}
